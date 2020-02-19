f0c9c0e81c58fe4408da96b81b6c967723cc9dd8
glautrou <g.lautrou@outlook.com>
2020-02-19 22:43:57 +0100

    Add default check

--- Startup.cs
+++ Startup.cs
@@ -24,6 +24,8 @@ namespace HealthCheckDemo
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddHealthChecks();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -51,6 +53,8 @@ namespace HealthCheckDemo
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+                endpoints.MapHealthChecks("/health");
             });
         }
     }
