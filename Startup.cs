226006519f4b3abca4f4f8cea9bd7497eb8f0ce3
glautrou <g.lautrou@outlook.com>
2020-02-24 22:35:52 +0100

    Add DirectoryExistsHealthCheck

--- Startup.cs
+++ Startup.cs
@@ -33,7 +33,8 @@ namespace HealthCheckDemo
                 .AddCheck<EvenSecondHealthCheck>("even_second")
                 .AddUrlGroup(new Uri("https://webnet.fr/"), "Webnet", HealthStatus.Degraded, new[] { "url" })
                 //.AddSqlServer("MyConnectionStrings");
-                .AddFileExistsHealthCheck(@"PathToFile", "My File", HealthStatus.Degraded, new[] { "server" });
+                .AddFileExistsHealthCheck(@"PathToFile", "My File", HealthStatus.Degraded, new[] { "server" })
+                .AddDirectoryExistsHealthCheck(@"PathToFolder", "My folder", HealthStatus.Degraded, new[] { "server" });
 
             services.AddHealthChecksUI();
         }
